7347e86dec568b21322d0b81b232b7250b651497
김소연 <syn.kim@lifesemantics.kr>
2022-01-20 18:00:10 +0900

    test.js 수정

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-const vdom = {};
+function createDOM(vdom) {
+  const element = document.createElement(vdom.tag);
+}
+
+const vdom = {
+  tag: "",
+  props: {},
+  children: [
+    {
+      tag: "",
+      props: {},
+      children: ["React 만들기"],
+    },
+  ],
+};
+
+createDOM(vdom);
